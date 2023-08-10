bd2ff6e82c8fb9f5e20afb8e4e9681b0ee7e282f
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-08-10 22:45:00 +0300

    hot fix

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
     case "get":
       const oneContact = await contacts.getContactById(id);
       return console.log(oneContact);
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
       return console.log(newContact);
+      break;
     case "remove":
       const deleteContact = await contacts.removeContact(id);
       return console.log(deleteContact);
